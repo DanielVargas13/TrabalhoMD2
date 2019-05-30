d41df5db8290e17caa2e30819a533281e35b724a
dalilamv <dalilamv99@outlook.com>
2019-05-29 21:21:22 -0300

    nova classe

---
+++ src/diofantina/Parcial.java
@@ -0,0 +1,60 @@
+package diofantina;
+
+public class Parcial {
+	
+	private int r, a, x, b, y;
+	
+	public Parcial() {
+		
+	}
+	
+	public Parcial(int r, int a, int x, int b, int y) {
+		setR(r);
+		setA(a);
+		setX(x);
+		setB(b);
+		setY(y);
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+
+}
